6857ce2a6a88ebe9b3d316cf6164e8edea2e4060
Nikhithareddy45 <nikkireddyperugu@gmail.com>
2025-11-29 15:46:06 +0530

    padding bottom

--- components/ui/SafeArea.tsx
+++ components/ui/SafeArea.tsx
@@ -4,7 +4,7 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 export default function SafeArea ({children}: React.PropsWithChildren<object>) {
     const insets = useSafeAreaInsets();
   return (
-    <View style={[styles.container, {paddingTop:insets.top}]} className=''>
+    <View style={[styles.container, {paddingTop:insets.top,paddingBottom:insets.bottom+5}]} className=''>
         {children}
     </View>
   )
